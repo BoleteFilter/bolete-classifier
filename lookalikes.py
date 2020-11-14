04a369b2bf9c7d1e0e732d5fc7c60a24312571e2
Liza <liza.bialik@gmail.com>
2020-11-13 22:11:17 -0500

    vectorized version of lookalikes

--- lookalikes.py
+++ lookalikes.py
@@ -2,37 +2,52 @@ import pandas as pd
 import numpy as np
 from collections import defaultdict
 
-features = pd.read_csv('./clean_data/features.csv')
-labels = pd.read_csv('./clean_data/labels.csv')
+labels = pd.read_csv("./clean_data/labels.csv")
 
-feats = [col for col in labels if col.startswith("F")]
+feats = [col for col in labels if col.startswith("F")][:-5]
 
-def get_feats(species_mid):
-    features = labels.loc[labels["mId"] == species_mid][feats]
+## Iterative version
+
+
+def get_feats(species_id):
+    features = labels.loc[labels["ID"] == species_id][feats]
     return features.to_numpy()
 
-def get_name(species_mid):
-    return labels.loc[labels["mId"] == species_mid]['NewName'].to_string()
 
-def distance(a, b):
-    return np.abs(np.sum(a-b))
+def get_name(species_id):
+    return labels.loc[labels["ID"] == species_id]["NewName"].to_string().split()[1]
 
-def lookalikes(species_mid, p):
-    species = get_feats(species_mid)
-    dists = defaultdict(int)
-    for spec_mid in labels['mId']:
-        if spec_mid is not species_mid:
-            other_species = get_feats(spec_mid)
-            dist = distance(species, other_species)
-            dists[spec_mid] = dist
-    l = (100.0001-p)/100*len(feats)
-    print(l)
-    result = [spec for (spec,dist) in dists.items() if dist <= l]
+
+def similarity(a, b):
+    return np.sum(a * b)
+
+
+def lookalikes(species_id, p):
+    species = get_feats(species_id)
+    sims = defaultdict(int)
+    for spec_id in labels["ID"]:
+        if spec_id is not species_id:
+            other_species = get_feats(spec_id)
+            sim = similarity(species, other_species)
+            sims[spec_id] = sim
+    result = [
+        spec for (spec, sim) in sims.items() if sim >= p * similarity(species, species)
+    ]
     return result
 
-results = lookalikes(313, 100)
+
+spec_id = 250
+results = lookalikes(spec_id, 0.9)
+print(spec_id, ", ", get_name(spec_id))
 for result in results:
     print(get_name(result))
+print()
 
 
 ## Vectorized Version ##
+p = 0.9
+Feats4All = labels.loc[:, feats].to_numpy()
+species = get_feats(spec_id)
+print(get_name(spec_id))
+idxs = np.where(Feats4All.dot(species.T) >= p * similarity(species, species))[0]
+print([get_name(idx + 1) for idx in idxs])
