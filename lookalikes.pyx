ac3c06235d30ac8207ba6cea1d94aebccc5252af
Jack Kenney <jnkenney@outlook.com>
2020-11-23 06:44:16 -0500

    working very fast cython lookalikes

--- lookalikes.pyx
+++ lookalikes.pyx
@@ -1,76 +1,46 @@
 from numpy.core.numeric import zeros_like
-import pandas as pd
 import numpy as np
 from collections import defaultdict
+import pandas as pd
 
-all_labels = pd.read_csv("./data_pipeline/clean_data/labels.csv")
-features = pd.read_csv("./data_pipeline/clean_data/features.csv")
-ids = pd.read_csv("./data_pipeline/clean_data/final_ids.csv")
-
-selected_ids = ids["OldID"].values
-selected_idxs = []
-for i in range(len(all_labels)):
-    if all_labels.loc[i]["ID"] in selected_ids:
-        selected_idxs.append(i)
-labels = all_labels.loc[selected_idxs]
-
-feats = [col for col in labels if col.startswith("F")][:-5]
-edibility = [col for col in labels if col.startswith("F")][-5:]
+features = np.loadtxt("./data_pipeline/clean_data/new_features.csv", delimiter=",")
+edibility = np.loadtxt("./data_pipeline/clean_data/new_edibility.csv", delimiter=",")
+names = pd.read_csv("./data_pipeline/clean_data/final_ids.csv")
 
 
 def get_num_species():
-    return len(selected_ids)
+    return edibility.shape[0]
 
 
-def get_old_id(new_id):
-    old_id = int(ids.loc[ids["NewID"] == new_id]["OldID"].values)
-    return old_id
+def get_feats(id):
+    return features[id]
 
 
-def get_feats(new_id):
-    species_id = get_old_id(new_id)
-    features = labels.loc[labels["ID"] == species_id][feats]
-    return features.to_numpy()
+def get_name(id):
+    return names.loc[names["NewID"] == id]["NewName"].to_string().split()[1]
 
 
-def get_name(new_id):
-    species_id = get_old_id(new_id)
-    return labels.loc[labels["ID"] == species_id]["NewName"].to_string().split()[1]
-
-
-def get_edibility(species_id):
-    species_id = get_old_id(species_id)
-    ed = np.argmax(labels.loc[labels["ID"] == species_id][edibility].values[0])
+def get_edibility(id):
+    ed = np.argmax(edibility[id])
     return ed
 
 
 def get_tau_edibility(tau_hat):
-    ed = np.zeros_like(tau_hat)
-    for i in range(len(tau_hat)):
-        ed[i] = get_edibility(tau_hat[i])
-    return ed
+    return np.argmax(edibility[tau_hat], axis=1)
 
 
 def similarity(a, b):
     return np.sum(a * b)
 
 
-## Vectorized Version ##
-Feats4All = labels.loc[:, feats].to_numpy()
-
-
 def lookalikes(spec_id, p):
-    species = get_feats(spec_id)
-    idxs = np.where(Feats4All.dot(species.T) >= p * similarity(species, species))[0]
+    u = get_feats(spec_id)
+    idxs = np.where(features.dot(u.T) >= p * similarity(u, u))[0]
     return idxs
 
 
-def species_from_feats(feats, p):
-    idxs = np.where(Feats4All.dot(feats.T) >= p * similarity(feats, feats))[0]
-    return idxs
-
-
-pd.set_option("display.max_colwidth", 100)
+def species_from_feats(u, p):
+    return np.where(features.dot(u.T) >= p * similarity(u, u))[0]
 
 
 def print_feats(species_id):
@@ -78,6 +48,6 @@ def print_feats(species_id):
     idxs = np.where(feats > 0)[1]
     print(idxs)
     for idx in idxs:
-        print(features.loc[features["id"] == idx + 1]["feature"].to_string()[-15:])
+        print(features[idx])
     print()
 
--- performance_concurrent.py
+++ performance_concurrent.py
@@ -1,8 +1,20 @@
+import numpy as np
+from data_utils import *
+from deco import *
+
+from lookalikes import (
+    species_from_feats,
+    lookalikes,
+    get_edibility,
+    get_tau_edibility,
+    get_num_species,
+)
+
 from locale import getpreferredencoding
 from data_utils import *
 import numpy as np
 
-# cython
+# Using cython lookalikes and deco for parallelized data processing
 from lookalikes import (
     species_from_feats,
     lookalikes,
@@ -12,6 +24,81 @@ from lookalikes import (
 )
 
 #######################################################################################
+# Performance from Evaluation
+#######################################################################################
+
+
+def compute_and_save_performance(name):
+    scores, y_pred, y_true, y_labels = load_raw_eval_data(name)
+
+    model_type = 0 if "characteristic" in name else 1 if "direct" in name else ""
+
+    ps, ps_ed = get_ps(scores, y_pred, y_labels, model_type)
+
+    save_performance_data(np.arange(0, 101) / 100, ps, name)
+    save_performance_data(np.arange(0, 101) / 100, ps_ed, name + "_ed")
+
+    return True
+
+
+@synchronized
+def get_ps(scores, y_pred, y_labels, model_type):
+    ps = np.zeros((101, len(y_labels)))
+    ps_ed = np.zeros((101, len(y_labels)))
+    for p in range(101):
+        for i in range(len(y_labels)):
+            ps[p, i] = get_performance_for_p(p, i, scores, y_pred, y_labels, model_type)
+            ps_ed[p, i] = get_performance_for_p(
+                p, i, scores, y_pred, y_labels, model_type, edibility=True
+            )
+    ps = np.mean(ps, axis=1)
+    ps_ed = np.mean(ps_ed, axis=1)
+    return ps, ps_ed
+
+
+@concurrent
+def get_performance_for_p(p, i, scores, y_pred, y_labels, model_type, edibility=False):
+    p = p / 100
+    perf = 0
+
+    if model_type == 0:  # characteristic
+        tau_hat = species_from_feats(y_pred[i], p)
+
+    elif model_type == 1:  # direct
+        i_scores = list(enumerate(scores[i]))
+        sorted_scores = sorted(i_scores, key=lambda x: x[1], reverse=True)
+        size_of_tau_hat = len(lookalikes(sorted_scores[0][0], p))
+        tau_hat = [idx for (idx, val) in sorted_scores[:size_of_tau_hat]]
+
+    else:
+        print("Unknown model type")
+        return
+
+    t = y_labels[i]
+    if not edibility:
+        perf = performance(tau_hat, t)
+    else:
+        perf = edibility_performance(tau_hat, t)
+
+    return perf
+
+
+def performance(tau_hat, t):
+    acc = t in tau_hat
+    M = get_num_species()
+    return ((M - len(tau_hat)) * acc) / M
+
+
+def edibility_performance(tau_hat, t):
+    real_ed = get_edibility(t)
+    tau_hat_ed = get_tau_edibility(tau_hat)
+    pred_ed = (  # mode predicted edibility
+        np.argmax(np.bincount(tau_hat_ed)) if len(tau_hat_ed) > 0 else -1
+    )
+    return 1 if real_ed == pred_ed else 0
+
+
+#######################################################################################
 # Random Characteristic Performance
 #######################################################################################
 
@@ -19,12 +106,12 @@ from lookalikes import (
 def random_char_performance(num_samples):
     name = "random_char"
 
-    ps = get_random_performance(num_samples, edibility=False)
-    ps = np.mean(ps, 2)
-    ps = np.mean(ps, 1)
-    save_performance_data(np.arange(0, 101) / 100, ps, name)
+    # ps = get_random_performance(num_samples, edibility=False)
+    # ps = np.mean(ps, 2)
+    # ps = np.mean(ps, 1)
+    # save_performance_data(np.arange(0, 101) / 100, ps, name)
 
-    print("species done")
+    # print("species done")
 
     ps_ed = get_random_performance(num_samples, edibility=True)
     ps_ed = np.mean(ps_ed, 1)
@@ -36,6 +123,7 @@ def random_char_performance(num_samples):
     return True
 
 
+@synchronized
 def get_random_performance(num_samples, edibility):
     C = 38
     results = np.zeros((101, num_samples, get_num_species()))
@@ -51,6 +139,7 @@ def get_random_performance(num_samples, edibility):
     return results
 
 
+@concurrent
 def get_performance(u, t, p, edibility):
     tau_hat = species_from_feats(u, p)
     # print(u)
@@ -87,16 +176,17 @@ def random_direct_performance(num_samples):
 
     print("species done")
 
-    ps_ed = get_random_direct_performance(num_samples, edibility=True)
-    ps_ed = np.mean(ps_ed, 1)
-    ps_ed = np.mean(ps_ed, 1)
-    save_performance_data(np.arange(0, 101) / 100, ps_ed, name + "_ed")
+    # ps_ed = get_random_direct_performance(num_samples, edibility=True)
+    # ps_ed = np.mean(ps_ed, 1)
+    # ps_ed = np.mean(ps_ed, 1)
+    # save_performance_data(np.arange(0, 101) / 100, ps_ed, name + "_ed")
 
-    print("edibility done")
+    # print("edibility done")
 
     return True
 
 
+@synchronized
 def get_random_direct_performance(num_samples, edibility):
     M = get_num_species()
     results = np.zeros((101, num_samples, M))
@@ -116,10 +206,12 @@ def get_random_direct_performance(num_samples, edibility):
     return results
 
 
+@concurrent
 def get_permuted_species_list(M):
     return np.random.permutation(np.arange(M))
 
 
+@concurrent
 def get_direct_performance(ids, t, p, edibility):
     size_of_tau_hat = len(lookalikes(ids[0], p))
     tau_hat = ids[:size_of_tau_hat]
