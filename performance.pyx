a890882a5d57ccbc3be18c77584214f4b05db5c5
Jack Kenney <jnkenney@outlook.com>
2020-11-23 07:19:46 -0500

    compile performance data and make final plots

--- performance.pyx
+++ performance.pyx
@@ -9,31 +9,108 @@ from lookalikes import (
     get_edibility,
     get_tau_edibility,
     get_num_species,
+    get_feats,
 )
+
+#######################################################################################
+# Performance from Evaluation
+#######################################################################################
+
+
+def compute_and_save_performance(name):
+    scores, y_pred, y_true, y_labels = load_raw_eval_data(name)
+
+    model_type = 0 if "characteristic" in name else 1 if "direct" in name else ""
+
+    ps, ps_ed = get_ps(scores, y_pred, y_labels, model_type)
+
+    save_performance_data(np.arange(0, 101) / 100, ps, name)
+    save_performance_data(np.arange(0, 101) / 100, ps_ed, name + "_ed")
+
+    return True
+
+
+def get_ps(scores, y_pred, y_labels, model_type):
+    ps = np.zeros((101, len(y_labels)))
+    ps_ed = np.zeros((101, len(y_labels)))
+    for p in range(101):
+        for i in range(len(y_labels)):
+            ps[p, i] = get_performance_for_p(p, i, scores, y_pred, y_labels, model_type)
+            ps_ed[p, i] = get_performance_for_p(
+                p, i, scores, y_pred, y_labels, model_type, edibility=True
+            )
+    ps = np.mean(ps, axis=1)
+    ps_ed = np.mean(ps_ed, axis=1)
+    return ps, ps_ed
+
+
+def get_performance_for_p(p, i, scores, y_pred, y_labels, model_type, edibility=False):
+    p = p / 100
+    perf = 0
+
+    if model_type == 0:  # characteristic
+        tau_hat = species_from_feats(y_pred[i], p)
+
+    elif model_type == 1:  # direct
+        i_scores = list(enumerate(scores[i]))
+        sorted_scores = sorted(i_scores, key=lambda x: x[1], reverse=True)
+        size_of_tau_hat = len(lookalikes(sorted_scores[0][0], p))
+        tau_hat = [idx for (idx, val) in sorted_scores[:size_of_tau_hat]]
+
+    else:
+        print("Unknown model type")
+        return
+
+    t = y_labels[i]
+    if not edibility:
+        perf = performance(tau_hat, t)
+    else:
+        perf = edibility_performance(tau_hat, t)
+
+    return perf
+
+
+def performance(tau_hat, t):
+    acc = t in tau_hat
+    M = get_num_species()
+    return ((M - len(tau_hat)) * acc) / M
+
+
+def edibility_performance(tau_hat, t):
+    real_ed = get_edibility(t)
+    tau_hat_ed = get_tau_edibility(tau_hat)
+    pred_ed = (  # mode predicted edibility
+        np.argmax(np.bincount(tau_hat_ed)) if len(tau_hat_ed) > 0 else -1
+    )
+    return 1 if real_ed == pred_ed else 0
+
 
 #######################################################################################
 # Random Characteristic Performance
 #######################################################################################
 
 
-def random_char_performance(num_samples):
+def random_char_performance(num_samples, type):
     name = "random_char"
 
-    ps = get_random_performance(num_samples, edibility=False)
-    ps = np.mean(ps, 2)
-    ps = np.mean(ps, 1)
-    save_performance_data(np.arange(0, 101) / 100, ps, name)
+    if type == "species":
+        ps = get_random_performance(num_samples, edibility=False)
+        ps = np.mean(ps, 2)
+        ps = np.mean(ps, 1)
+        save_performance_data(np.arange(0, 101) / 100, ps, name)
 
-    print("species done")
+        print("species done")
+        return True
 
-    ps_ed = get_random_performance(num_samples, edibility=True)
-    ps_ed = np.mean(ps_ed, 1)
-    ps_ed = np.mean(ps_ed, 1)
-    save_performance_data(np.arange(0, 101) / 100, ps_ed, name + "_ed")
+    if type == "edibility":
+        ps_ed = get_random_performance(num_samples, edibility=True)
+        ps_ed = np.mean(ps_ed, 1)
+        ps_ed = np.mean(ps_ed, 1)
+        save_performance_data(np.arange(0, 101) / 100, ps_ed, name + "_ed")
 
-    print("edibility done")
+        print("edibility done")
 
-    return True
+        return True
 
 
 def get_random_performance(num_samples, edibility):
@@ -77,24 +154,27 @@ def get_performance(u, t, p, edibility):
 #######################################################################################
 
 
-def random_direct_performance(num_samples):
+def random_direct_performance(num_samples, type):
     name = "random_direct"
 
-    ps = get_random_direct_performance(num_samples, edibility=False)
-    ps = np.mean(ps, 2)
-    ps = np.mean(ps, 1)
-    save_performance_data(np.arange(0, 101) / 100, ps, name)
+    if type == "species":
+        ps = get_random_direct_performance(num_samples, edibility=False)
+        ps = np.mean(ps, 2)
+        ps = np.mean(ps, 1)
+        save_performance_data(np.arange(0, 101) / 100, ps, name)
 
-    print("species done")
+        print("species done")
+        return True
 
-    ps_ed = get_random_direct_performance(num_samples, edibility=True)
-    ps_ed = np.mean(ps_ed, 1)
-    ps_ed = np.mean(ps_ed, 1)
-    save_performance_data(np.arange(0, 101) / 100, ps_ed, name + "_ed")
+    if type == "edibility":
+        ps_ed = get_random_direct_performance(num_samples, edibility=True)
+        ps_ed = np.mean(ps_ed, 1)
+        ps_ed = np.mean(ps_ed, 1)
+        save_performance_data(np.arange(0, 101) / 100, ps_ed, name + "_ed")
 
-    print("edibility done")
+        print("edibility done")
 
-    return True
+        return True
 
 
 def get_random_direct_performance(num_samples, edibility):
@@ -142,4 +222,32 @@ def get_direct_performance(ids, t, p, edibility):
 
 
 #######################################################################################
+# Perfect Species Performance
+#######################################################################################
 
+
+def perfect_performance(type):
+
+    if type == "species":
+        ps = get_perfect_performance(False)
+        save_performance_data(np.arange(0, 101) / 100, ps, "perfect")
+        return True
+
+    if type == "edibility":
+        ps_ed = get_perfect_performance(True)
+        save_performance_data(np.arange(0, 101) / 100, ps_ed, "perfect_ed")
+
+        return True
+
+
+def get_perfect_performance(edibility):
+    results = np.zeros((101, get_num_species()))
+
+    for p in range(101):
+        print("=", end="")
+        for t in range(get_num_species()):
+            u = get_feats(t)
+            results[p, t] = get_performance(u, t, p / 100, edibility)
+    print("done")
+    results = np.mean(results, axis=1)
+    return results
--- performance_driver.py
+++ performance_driver.py
@@ -1,5 +1,23 @@
 from performance import random_direct_performance, random_char_performance
 
+
+def drive(num_samples):
+    print("Direct:")
+
+    print("Species")
+    random_direct_performance(num_samples, "species")
+
+    print("Edibility")
+    random_direct_performance(num_samples, "edibility")
+
+    print("Characteristics:")
+
+    print("Species")
+    random_char_performance(num_samples, "species")
+
+    print("Edibility")
+    random_char_performance(num_samples, "edibility")
+
+
 if __name__ == "__main__":
-    # random_direct_performance(100)
-    random_char_performance(100)
+    drive(100)
